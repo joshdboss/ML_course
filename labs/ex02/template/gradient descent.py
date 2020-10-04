b6a35be9ac0a1521ffa1800fb0529a565a8e7775
joshdboss <newbie856@gmail.com>
2020-10-04 13:07:01 +0200

    finished lab 2

--- labs/ex02/template/gradient descent.py
+++ labs/ex02/template/gradient descent.py
@@ -1,32 +1,95 @@
 # -*- coding: utf-8 -*-
 """Gradient Descent"""
 
-def compute_gradient(y, tx, w):
-    """Compute the gradient."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute gradient and loss
-    # ***************************************************
-    raise NotImplementedError
+def compute_gradient_mse(y, tx, w):
+    """ COMPUTE_GRADIENT
+        Computes the gradient of the MSE of a model applied on a set
 
+        INPUTS:
+        y: an Nx1 array of the output variable
+        tx: an Nxd array of the input variable
+        w: a dx1 array of the model
 
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """Gradient descent algorithm."""
+        OUTPUTS:
+        A dx1 array of the gradient 
+    """
+    return -1/len(y) * tx.T @ (y - tx @ w)
+
+    
+def compute_gradient_mae(y, tx, w):
+    """ COMPUTE_GRADIENT
+        Computes the gradient of the MAE of a model applied on a set
+
+        INPUTS:
+        y: an Nx1 array of the output variable
+        tx: an Nxd array of the input variable
+        w: a dx1 array of the model
+
+        OUTPUTS:
+        A dx1 array of the gradient 
+    """
+    return -1/len(y) * np.sign(y - tx @ w) @ tx
+    
+    
+def gradient_descent_mse(y, tx, initial_w, max_iters, gamma):
+    """ GRADIENT_DESCENT
+        Iterates and finds the best model using gradient descent and MSE
+
+        INPUTS:
+        y: an Nx1 array of the output variable
+        tx: an Nxd array of the input variable
+        initial_w: a dx1 array of the initial model to use
+        max_iters: a scalar representing the maximum number of iterations
+        gamma: a scalar representing the step size/learning rate
+
+        OUTPUTS:
+        losses: a max_iters dimensional array representing losses for iterations
+        ws: a max_iters dimensional array representing models over iterations
+    """
     # Define parameters to store w and loss
     ws = [initial_w]
     losses = []
     w = initial_w
     for n_iter in range(max_iters):
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: compute gradient and loss
-        # ***************************************************
-        raise NotImplementedError
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: update w by gradient
-        # ***************************************************
-        raise NotImplementedError
+        # compute the gradient and loss given the current w
+        grad = compute_gradient_mse(y, tx, w)
+        loss = compute_loss_mse(y, tx, w)
+        # update w by gradient descent
+        w = w - gamma*grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
+
+    
+def gradient_descent_mae(y, tx, initial_w, max_iters, gamma):
+    """ GRADIENT_DESCENT
+        Iterates and finds the best model using gradient descent and MAE
+
+        INPUTS:
+        y: an Nx1 array of the output variable
+        tx: an Nxd array of the input variable
+        initial_w: a dx1 array of the initial model to use
+        max_iters: a scalar representing the maximum number of iterations
+        gamma: a scalar representing the step size/learning rate
+
+        OUTPUTS:
+        losses: a max_iters dimensional array representing losses for iterations
+        ws: a max_iters dimensional array representing models over iterations
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute the gradient and loss given the current w
+        grad = compute_gradient_mae(y, tx, w)
+        loss = compute_loss_mae(y, tx, w)
+        # update w by gradient descent
+        w = w - gamma*grad
         # store w and loss
         ws.append(w)
         losses.append(loss)
