7a30f2be05593a2d79734b22bf9c4744580d9f4c
joshdboss <newbie856@gmail.com>
2020-10-13 11:35:18 +0200

    finished lab 3

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -5,11 +5,18 @@ import numpy as np
 
 
 def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    """ BUILD_POLY Polynomial basis functions for input data x, for j=0 up to j=degree
+        Takes an input vector x and returns a polynomial basis of degree j=degree
+
+        INPUTS:
+        x (N x 1): an array of the output variable with only 1 feature
+        degree: The highest degree of the basis
+
+        OUTPUTS:
+        basis (N x degree): a polynomial basis on the output variable
+    """
+    x_expanded = np.tile(x,[degree+1,1]).T #expands the variable to degree of basis
+    powers = np.tile(np.arange(0,degree+1), [x.shape[0],1]) #create the array of powers to raise
+    basis = np.power(x_expanded, powers)
+    
+    return basis
