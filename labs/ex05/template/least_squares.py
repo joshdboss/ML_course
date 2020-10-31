d7c98ce71764045f26bb643bb3d6d737b6beb3f0
joshdboss <newbie856@gmail.com>
2020-10-31 09:40:05 +0100

    saved changes

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -1,17 +1,34 @@
-# -*- coding: utf-8 -*-
-"""Exercise 3.
-
-Least Square
-"""
-
 import numpy as np
 
+def compute_loss_mse(y, tx, w):
+    """ COMPUTE_LOSS_MSE
+        Calculates the loss of a dataset using MSE.
+
+        INPUTS:
+        y: an Nx1 array of the output variable
+        tx: an Nxd array of the input variable
+        w: a dx1 array of the model
+
+        OUTPUTS:
+        A scalar of the mean square error of the model applied on the dataset
+    """
+    return sum((y - tx @ w)**2)/len(y)
 
 def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    """ LEAST_SQUARES Calculates the least squares solution
+        Calculates the least squares solution to a given dataset
+        Returns the mean square error and optimal weights
+        
+        INPUTS
+        y (Nx1): array of the output data with N datapoints
+        tx (NxD): array of the input data with N datapoints and D characteristics
+        
+        OUTPUTS
+        w: the optimal weights for the given system
+        MSE: A scalar of the mean square error of the model
+    """
+    
+    w = np.linalg.inv(tx.T @ tx) @ tx.T @ y
+    MSE = compute_loss_mse(y, tx, w)
+    
+    return w, MSE
